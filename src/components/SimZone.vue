f85b9596a0d118ecb3da445b921898a11664a772
mcintydeac <mcintydeac@ecs.vuw.ac.nz>
2019-04-26 19:16:51 +1200

    Started creating the CA

--- src/components/SimZone.vue
+++ src/components/SimZone.vue
@@ -1,35 +1,30 @@
 <template>
-  <div class="hello">
+  <div>
+    <tr v-for="(row, idx1) in grid" :key="idx1">
+      <td v-for="(col, idx2) in row" :key="idx2">
+        <div class="cell" :style="{'background-color': grid[idx1][idx2]}"></div>
+      </td>
+    </tr>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-
-@Component
-export default class SimZone extends Vue {
-  @Prop() private msg!: string;
-}
+<script>
+export default {
+  data: () => ({
+    grid: [
+      ["#1df954", "#1CA085", "#3ba07e", "#1CA085", "#1df954"],
+      ["#1df954", "#1CA085", "#3ba07e", "#1CA085", "#1df954"],
+      ["#5a704d", "#1CA085", "#3ba07e", "#1CA085", "#1df954"],
+      ["#1df954", "#1CA085", "#3ba07e", "#d21ad8", "#1df954"],
+      ["#1df954", "#1CA085", "#3ba07e", "#1CA085", "#1df954"]
+    ]
+  })
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-.hello{
-  color: beige;
-  background-color: beige;
-}
+  .cell{
+    width: 20px;
+    height: 20px;
+  }
 </style>
