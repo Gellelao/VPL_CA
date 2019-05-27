01893166c65ddae733bbbf755aaaea1a0519610f
Deacon McIntyre <mcintydeac@ecs.vuw.ac.nz>
2019-05-27 16:48:10 +1200

    Change config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/~mcintydeac/'
+    : '/',
+  outputDir: '../public_html',
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "scss",
