391f8aabd566768dc1ea1c37e93b8a216b6e1855
mcintydeac <mcintydeac@ecs.vuw.ac.nz>
2019-07-25 21:39:53 +1200

    Allow mouse to be further up for deletion, change === to ==

--- src/components/RulesZone.vue
+++ src/components/RulesZone.vue
@@ -222,7 +222,7 @@ export default {
 
         index = sourceId.lastIndexOf("_");
         sourceId = sourceId.substr(0, index);
-        let source = this.blocks.stateBlocks.find(x => x.id === sourceId);
+        let source = this.blocks.stateBlocks.find(x => x.id == sourceId);
         var stateColour = source.colour;
         var requiredState = cond.requiredState;
         var operator = cond.operator;
@@ -269,42 +269,42 @@ export default {
     // Set up events to make sure changes down in the components are reflected in the data up here
     this.$root.$on("updateStateColour", data => {
       Vue.set(
-        this.blocks.stateBlocks.find(x => x.id === data.id),
+        this.blocks.stateBlocks.find(x => x.id == data.id),
         "colour",
         data.colour
       );
     });
     this.$root.$on("updateConditionRequiredState", data => {
       Vue.set(
-        this.blocks.conditionBlocks.find(x => x.id === data.id),
+        this.blocks.conditionBlocks.find(x => x.id == data.id),
         "requiredState",
         data.requiredState
       );
     });
     this.$root.$on("updateConditionNeighbours", data => {
       Vue.set(
-        this.blocks.conditionBlocks.find(x => x.id === data.id),
+        this.blocks.conditionBlocks.find(x => x.id == data.id),
         "desiredNumberOfNeighbours",
         data.neighbours
       );
     });
     this.$root.$on("updateConditionOperator", data => {
       Vue.set(
-        this.blocks.conditionBlocks.find(x => x.id === data.id),
+        this.blocks.conditionBlocks.find(x => x.id == data.id),
         "operator",
         data.operator
       );
     });
     this.$root.$on("updateActionDesiredState", data => {
       Vue.set(
-        this.blocks.actionBlocks.find(x => x.id === data.id),
+        this.blocks.actionBlocks.find(x => x.id == data.id),
         "desiredState",
         data.desiredState
       );
     });
     this.$root.$on("updateNeighbourhood", data => {
       Vue.set(
-        this.blocks.transformBlocks.find(x => x.id === data.id),
+        this.blocks.transformBlocks.find(x => x.id == data.id),
         "neighbourhood",
         data.neighbourhood
       );
@@ -334,7 +334,8 @@ export default {
         },
         drag: params => {
           this.mouseOverTrash =
-            Math.hypot(params.e.clientX, params.e.clientY - 64) <= 80;
+            Math.hypot(params.e.clientX, params.e.clientY - 64) <= 80 ||
+            (params.e.clientX <= 0 && params.e.clientY < 64);
         },
         stop: params => {
           this.trashOpen = false;
